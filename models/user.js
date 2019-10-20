f25e0bf4d1a5a00e3a0e1a55aa90c57de3b197aa
Jacob Moss <jacobmoss09@gmail.com>
2019-10-20 17:41:02 -0500

    user model added

---
+++ models/user.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    lastName: DataTypes.STRING,
+    caseNumber: DataTypes.INTEGER
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
