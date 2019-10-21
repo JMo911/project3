65b6c6aa8af02dd978ecdd29137bf886ab5c456b
Jacob Moss <jacobmoss09@gmail.com>
2019-10-20 19:30:01 -0500

    user post route working as expected

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express')
 const app = express()
 var db = require("./models");
 
+// app.use(express.bodyParser())
+app.use(express.json());
+
 var PORT = process.env.PORT || 8080;
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
   });
 
-// app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/api/users', (req, res) => {
+    // res.send('Hello World!');
+    // const user = req.body;
+    // console.log(req.body);
+    db.User.create(req.body).then(function(dbUser) {
+        res.json(dbUser);
+      });
+});
 
 // app.listen(port, () => console.log(`Example app listening on port ${port}!`));
