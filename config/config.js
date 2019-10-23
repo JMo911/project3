96964dd377797188ad29bc75bea951b389400f17
Jacob Moss <jacobmoss09@gmail.com>
2019-10-22 20:37:17 -0500

    environment variables defined for db connection

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "root",
-    "database": "project3",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql",
-    "port": 8889
+    "port": process.env.MYSQL_PORT
   },
   "test": {
     "username": "root",
